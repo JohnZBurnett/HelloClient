1aa9175286647fb7464151ff52ba92d80df646d1
JohnZBurnett <tuninator@gmail.com>
2018-10-26 12:12:10 -0400

    finish ConLimitFilter

--- src/ConLimitFilter.java
+++ src/ConLimitFilter.java
@@ -10,9 +10,23 @@ public class ConLimitFilter implements Filter {
 	public void init( FilterConfig filterConfig ) throws ServletException
 	{
 		String s = filterConfig.getInitParameter("limit");
-		if ( s == null ) {
+		if ( s == null )
 			throw new ServletException("Missing init parameter:" + limit);
 		limit = Integer.parseInt( s ); 
+		
+	}
+	
+	public void doFilter( ServletRequest req, ServletResponse res, FilterChain chain ) throws IOException, ServletException
+	{
+		if ( count > limit ) {
+			HttpServletResponse httpRes = (HttpServletResponse)res; 
+			httpRes.sendError(httpRes.SC_SERVICE_UNAVAILABLE, "Too Busy.");
+		} else {
+			++count;
+			chain.doFilter( req,  res);
+			--count; 
 		}
 	}
+	
+	public void destroy() {}
 }
